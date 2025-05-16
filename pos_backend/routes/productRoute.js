a72483b5bb2917d0d1153d0384f0e75926409ab8
Ravindu Dilhara <r.dilhara@h2compute.com>
2025-05-16 10:55:15 +0530

    product delete method in backend

--- pos_backend/routes/productRoute.js
+++ pos_backend/routes/productRoute.js
@@ -4,11 +4,13 @@ const {
   addProduct,
   getProductsByCategory,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 const router = express.Router();
 
 router.route("/").post(isVerifiedUser, addProduct);
 router.route("/:id").get(isVerifiedUser, getProductsByCategory);
 router.route("/:id").put(isVerifiedUser, updateProduct);
+router.route("/:id").delete(isVerifiedUser, deleteProduct);
 
 module.exports = router;
