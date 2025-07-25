b47e791b9a7826fb6273bc9b782770650a451398
EA-Gayan <janith.gayan141@gmail.com>
2025-07-25 10:59:43 +0530

    backend fixes

--- pos_backend/models/productModel.js
+++ pos_backend/models/productModel.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    sname: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String },
     category: {
